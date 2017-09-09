2de51b1c6d79a06c3e7e672703bce080588479df
pengcheng <pengcheng@megvii.com>
2017-09-09 21:15:44 +0800

    fix: 1.44 wrong lambda expression

--- 1.44.smoothRepeated.rkt
+++ 1.44.smoothRepeated.rkt
@@ -1,24 +1,23 @@
 #lang planet neil/sicp
+;from previous code:
 (define (compose f g)
   (lambda  (x ) ( f ( g x))))
 
 (define  (repeat f n)
   ( if (= 1  n )  f
   (lambda (x ) ((compose  f  (repeat f (- n 1)))  x))))
-
+;
 (define dx 1)
-
 (define (smooth  f )
    (lambda (x)  (/ (+ (f (- x dx))
                       (f x)
                       (f (+ x dx)))
                    3))
    )
+(define (resmoothn f n)
+  (lambda (x) (((repeat smooth n) f) x)))
 
-(define (smooth-n f n)
-  (repeat smooth n) f)
-
+;test:
 (define ( sqr x)
   (* x x))
-
-((smooth-n sqr 1000) 10.0)
+((resmoothn sqr 15) 5.0)
